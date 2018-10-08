87b9657cb3f46f6fb9dd47380db3a5d9c44710ba
David masterson <david73180@gmail.com>
2018-10-08 03:05:23 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,7 @@ def monopoly
 					 														b_and_o_railroad: {},
 					 														shortline: {}
 				 																},
-																				
+
 						  :rent_in_dollars => {
 																	one_piece_owned: 25,
 																	two_pieces_owned: 50,
